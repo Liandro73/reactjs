569ae48fa5d605695c185d1489fb04fd5582d3cb
Dernival Liandro <liandro.sys@gmail.com>
2020-06-23 19:57:41 -0300

    Ciclo de vida, e ecossistemas do ReactJS

--- src/index.js
+++ src/index.js
@@ -1,32 +1,68 @@
-import React from "react"
+import React, { Component } from "react"
 import ReactDOM from "react-dom"
-import Button from "./Button"
-import ComponentA from "./ComponentA"
-import ComponentB from "./ComponentB"
+// import Button from "./Button"
+// import ComponentA from "./ComponentA"
+// import ComponentB from "./ComponentB"
 import "./styles.css"
 
-function soma(a, b) {
-    alert(a + b)
+class App extends Component {
+    
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            clock: 1000,
+            cup: 'água'
+        }
+    }
+
+    componentDidMount() {
+        window.setTimeout(() => {
+            this.setState({
+                cup: 'suco de maracujá'
+            })
+        }, 3000)
+    }
+
+    modifyCup = () => {
+        this.setState({
+            cup: 'café'
+        })
+    }
+
+    render() {
+        const { clock, cup } = this.state
+        return(
+            <div>
+                <h1>{clock}</h1>
+                <button onClick={() => this.modifyCup()} >{cup}</button>
+            </div>
+        )
+    }
 }
 
-const App = () => {
+// function soma(a, b) {
+//     alert(a + b)
+// }
 
-    return (
-        <div className="App">
-            <div>
-                Hello World
-            </div>
-            <div>
-                <Button onClick={() => soma(10, 20)} name="Dernival Liandro" />
-                <ComponentA>
-                    <ComponentB>
-                        <Button onClick={() => soma(40, 20)} name="Alícia Liandro" />
-                    </ComponentB>
-                </ComponentA>
-            </div>
-        </div>
-    )
-}
+// const App = () => {
+
+//     return (
+//         <div className="App">
+//             <div>
+//                 Hello World
+//             </div>
+//             <div>
+//                 <Button onClick={() => soma(10, 20)} name="Dernival Liandro" />
+//                 <ComponentA>
+//                     <ComponentB>
+//                         <Button onClick={() => soma(40, 20)} name="Alícia Liandro" />
+//                     </ComponentB>
+//                 </ComponentA>
+//             </div>
+//         </div>
+//     )
+// }
 
 const rootElement = document.getElementById("root")
 ReactDOM.render(<App />, rootElement)
