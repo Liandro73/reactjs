0be610f9a84659d07191906567fa2d45063d4ed8
Dernival Liandro <liandro.sys@gmail.com>
2020-06-23 13:02:16 -0300

    JSX e renderização de elementos

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./styles.css"
 
+function sum(a, b) {
+    return a + b;
+}
+
+function primeiroJSX() {
+    return (
+        <div className="teste">
+            Dernival Liandro - Introdução ao ReactJS
+            <h3>Soma: {sum(10, 20)}</h3>
+        </div>
+    )
+}
+
 const App = () => {
 
     return (
         <div className="App">
             Hello World
+            {primeiroJSX()}
         </div>
     )
 }
